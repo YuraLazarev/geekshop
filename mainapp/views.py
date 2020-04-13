b9b4a9c979b4135a639db1b1fd5d0aef11c0e517
Yura Lazarev <yuralazarev@gmail.com>
2020-04-13 15:20:40 +0300

    ORM use in templates and view

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def main(request):
+    products = Product.objects.all()[:4]
     value = {
         'title': 'Главная',
-        'content_class': 'slider'
+        'content_class': 'slider',
+        'products': products
     }
     return render(request, 'index.html', value)
 
@@ -25,22 +28,27 @@ def test(request):
     }
     return render(request, 'test.html', value)
 
-def products(request):
-    prod_menu = [
-        {'href':'products_all', 'name':'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+def products(request, pk=None):
+    if pk:
+        product_item = Product.objects.get(pk=pk)
+
+    # prod_menu = [
+    #     {'href':'products_all', 'name':'все'},
+    #     {'href': 'products_home', 'name': 'дом'},
+    #     {'href': 'products_office', 'name': 'офис'},
+    #     {'href': 'products_modern', 'name': 'модерн'},
+    #     {'href': 'products_classic', 'name': 'классика'},
+    # ]
     value = {
-        'links_menu': prod_menu,
+        #'links_menu': prod_menu,
         'title': 'Каталог',
-        'content_class': 'hero-white'
+        'content_class': 'hero-white',
+        'product_item': product_item
     }
     return render(request, 'products.html', value)
 
-def products_all(request):
+def products_all(request, pk=None):
+
     prod_menu = [
         {'href':'products_all', 'name':'все'},
         {'href': 'products_home', 'name': 'дом'},
